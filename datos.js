03dd544bf1af5fa5886bf48ca587d3264a6209cf
RolanditoDev <rolando.palma.rivero@gmail.com>
2026-08-14 05:54:12 -0400

    Versión para la Agencia 03020. New file datos.js.

---
+++ datos.js
@@ -0,0 +1,13 @@
+const CODE_AGENCIA = '03130';
+const NAME_AGENCIA = 'El Tigre Centro';
+const ADDRESS_AGENCIA = '2da Carrera Sur, Edificio Hana detrás del SENIAT';
+const PLAZO_AGENCIA = '7 días';
+
+
+
+
+
+const CODE_AGENCIA = '03020';
+const NAME_AGENCIA = 'El Tigre';
+const ADDRESS_AGENCIA = 'Av. Francisco de Miranda con Calle 18 Sur, Edificio Los Geranios al lado de la Panadería Siria';
+const PLAZO_AGENCIA = '7 días';
